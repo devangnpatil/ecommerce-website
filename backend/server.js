c52d0d66de9a32e4532d0853b0070c884972f52e
Devang Patil <devangnpatil@gmail.com>
2020-10-03 12:21:51 +0530

    Create models

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 dotenv.config();
+
+connectDB();
+
 const app = express();
 
 app.get('/', (req, res) => {
@@ -21,5 +26,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   5000,
-  console.log(`Server running in ${process.env.NODE_ENV} port 5000 ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} port 5000 ${PORT}`.yellow.bold
+  )
 );
