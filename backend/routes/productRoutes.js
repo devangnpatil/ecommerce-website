275d9300b61d568e35ab1c79912161b26f01d7b8
Devang Patil <devangnpatil@gmail.com>
2020-10-04 09:32:32 +0530

    add user controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,33 +1,11 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
+import {
+  getProducts,
+  getProductById,
+} from '../controller/productController.js';
 const router = express.Router();
-import Product from '../models/productModel.js';
 
-// @desc Fetch all products
-// @rout GET /api/products
-// @access Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-// @desc Fetch single product
-// @rout GET /api/product
-// @access Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
