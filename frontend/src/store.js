531ee6bcc9bdac6a098a7eea5d715a7dcdb25fb8
Devang Patil <devangnpatil@gmail.com>
2020-10-04 11:26:23 +0530

    create user info store and actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,21 +6,31 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers.js';
 
+import { userLoginReducer } from './reducers/userReducers';
+
 import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : null;
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage,
+  },
+  userLogin: {
+    userInfo: userInfoFromStorage,
   },
 };
 
